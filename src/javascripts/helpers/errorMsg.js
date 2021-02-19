1ff237be5d4ada04fa6e16fb9db865a2e865c8c2
Dani Crosby <dani.crosby1111@gmail.com>
2021-02-19 10:58:34 -0600

    bear form refactored to js file

---
+++ src/javascripts/helpers/errorMsg.js
@@ -0,0 +1,12 @@
+// const errorMessage = (student) => {
+//   if (student) {
+//     document.querySelector('#error-message').innerHTML = '';
+//   } else {
+//     document.querySelector('#error-message').innerHTML = `
+//       <div style="color: red;margin-bottom: 10px;">
+//         <b>Please enter info</b>
+//       </div>`;
+//   }
+// };
+
+// export default errorMessage;
