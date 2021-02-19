ebd8cc2428835a2e497041dacf7124cc5fbbc641
Ben Jarrett <b.cl.jarrett@gmail.com>
2021-02-18 20:47:15 -0600

    Created setup branch with webframe installed

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,7 +2,14 @@ import '../styles/main.scss';
 
 const init = () => {
   $('#app').html('<h1>HELLO! You are up and running!</h1>');
-  console.log('YOU ARE UP AND RUNNING!');
+  console.warn('YOU ARE UP AND RUNNING!');
 };
 
 init();
+
+// To Do: //
+// 1. Create Setup Branch. Add, commit, push, pull down //
+// 2. Create New Branch //
+// 3. Create PrintToDom Function //
+// 4. Create Array with Data Strcutre for Cards //
+// 5. Create Bear Form //
