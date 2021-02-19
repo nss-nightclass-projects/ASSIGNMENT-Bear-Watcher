6d8df0174252eddeaaf586de7c822e47ef02defe
elliewalkersm <elliewalkersm@gmail.com>
2021-02-18 20:21:01 -0600

    starting form

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,27 @@
 import '../styles/main.scss';
 
+const printToDom = (divId, textToPrint) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = textToPrint;
+};
+
+const bearForm = () => {
+  cost domstring += `<form>
+    <div class="form-group">
+      <label for="bearName">Enter Bear Name</label>
+      <input type="text" class="form-control" id="bearName">
+    </div>
+    <div class="form-group">
+      <label for="bearImage"></label>
+      <input type="url" class="form-control" id="bearImage">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>`
+  printToDom('#bearForm', domString);
+};
+
 const init = () => {
-  $('#app').html('<h1>HELLO! You are up and running!</h1>');
-  console.log('YOU ARE UP AND RUNNING!');
+  bearForm();
 };
 
 init();
