22e733fd71a916ab0edbaf7968a39da2fe0b3b77
Greenfin17 <jmaple@yahoo.com>
2021-02-20 17:01:13 -0600

    recording fishing attempts and successes

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -10,13 +10,16 @@ const listenNewBear = (e) => {
   if (e.target.id === 'bear-submit') {
     const bearName = document.getElementById('bearName').value;
     const bearUrl = document.getElementById('bearUrl').value;
+    const fishingAttempts = [];
     const newBear = {
       bearIndex,
       bearName,
-      bearUrl
+      bearUrl,
+      fishingAttempts
     };
     bearData.push(newBear);
-    console.warn(bearData);
+    // console.warn(bearData);
+    // console.warn(time);
     bearIndex += 1;
     document.getElementById('bearName').value = '';
     document.getElementById('bearUrl').value = '';
@@ -24,8 +27,48 @@ const listenNewBear = (e) => {
   }
 };
 
+const fishingAttempt = (caught, bearId) => {
+  const attempTime = new Date();
+  const timeStamp = attempTime.toString();
+  const newAttempt = {
+    caught,
+    timeStamp
+  };
+  bearData[bearId].fishingAttempts.push(newAttempt);
+};
+
+const listenFishing = (e) => {
+  // bear id if fishing button clicked
+  const bearId = e.target.parentNode.parentNode.id;
+  // bear id if missed or caught button clicked
+  const fishingBearId = e.target.parentNode.parentNode.parentNode.id;
+  const fishingResult = document.getElementById(`fishing-result_${bearId}`);
+  console.warn(e.target.id);
+  // console.warn(fishingResult.style);
+  switch (e.target.id) {
+    case `fish-button_${bearId}`:
+      console.warn('fish button');
+      // fishingResult.classList.remove('collapse');
+      fishingResult.classList.toggle('collapse');
+      break;
+    case `caught_${fishingBearId}`:
+      console.warn('caught');
+      fishingAttempt(true, fishingBearId);
+      river();
+      break;
+    case `missed_${fishingBearId}`:
+      console.warn('missed');
+      fishingAttempt(false, fishingBearId);
+      river();
+      break;
+    default:
+      break;
+  }
+};
+
 const buttonListeners = () => {
   document.getElementById('bear-form').addEventListener('click', listenNewBear);
+  document.getElementById('bear-river').addEventListener('click', listenFishing);
 };
 
 const init = () => {
--- src/javascripts/modules/river.js
+++ src/javascripts/modules/river.js
@@ -1,17 +1,36 @@
 import paintDom from './paintDom';
 import bearData from '../../assets/data/bearData';
 
+const calcFishCaught = (attempts) => {
+  let sum = 0;
+  attempts.forEach((item) => {
+    if (item.caught) {
+      sum += 1;
+    }
+  });
+  return sum;
+};
+
 const river = () => {
   let bearString = '';
   bearData.forEach((item) => {
+    const fishCaught = calcFishCaught(item.fishingAttempts);
     bearString += `
       <div class="card" id=${item.bearIndex} style="width: 18rem;">
         <img src="${item.bearUrl}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${item.bearName}</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <button class="btn btn-primary">Fish Attempt</>
-          <button class="btn btn-primary">Fish Caught</>
+          <p class="fish">Fish attempts: ${item.fishingAttempts.length}</p>
+          <p class="fish">Fish caught: ${fishCaught}</p>
+          <button class="btn btn-primary" type="button" data-toggle="collapse" 
+                  data-target="#fishing-result_${item.bearIndex}" aria-expanded="false" 
+                  aria-controls="collapseExample" id="fish-button_${item.bearIndex}">
+          Button with data-target
+          </button>
+          <div class="collapse" id="fishing-result_${item.bearIndex}">
+            <button class="btn btn-primary caught-fish" id="caught_${item.bearIndex}">Caught fish!</>
+            <button class="btn btn-primary missed-fish" id="missed_${item.bearIndex}">Missed fish</>
+          </div>
         </div>
       </div>`;
   });
