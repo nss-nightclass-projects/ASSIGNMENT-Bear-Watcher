6f8c58d66373656dd9f4c5d71f91e442aa65d64b
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-02-20 12:09:21 -0600

    added a function to print everything to DOM

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,10 +1,12 @@
 import '../styles/main.scss';
 import bearCardPrint from './components/bearCardPrint';
 import bearFormPrint from './components/bearFormPrint';
+import printApp from './components/printApp';
 import domEvents from './events/domEvents';
 import bears from './helpers/data/bearData';
 
 const init = () => {
+  printApp();
   bearFormPrint();
   bearCardPrint(bears);
   domEvents();
