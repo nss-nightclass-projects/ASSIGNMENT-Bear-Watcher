410432322593e089d650fdc11da42414a2bdbd0e
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-02-21 17:14:09 -0600

    MVP is finished

--- src/javascripts/components/jumbotron.js
+++ src/javascripts/components/jumbotron.js
@@ -1,12 +1,12 @@
 const jumbotron = () => {
   document.querySelector('#jumbotron').innerHTML = `
-    <h1 class="display-4">Brooks Falls - Brown Bears Tracker</h1>
-    <p class="lead">
+    <h1 class='display-4'>Brooks Falls - Brown Bears Tracker</h1>
+    <p class='lead'>
       Welcome researchers, scientists and bear watchers! Here you can monitor
       the activity of your favorite brown bears located in Brooks Falls -
       Katmai National Park, Alaska, USA.
     </p>
-    <hr class="my-4" />`;
+    <hr class='my-4' />`;
 };
 
 export default jumbotron;
