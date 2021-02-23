cc5dcde61551e0e8cde529361c85de9b8f4bfbf4
Dani Crosby <dani.crosby1111@gmail.com>
2021-02-22 23:07:42 -0600

    started fish tally, updated readme

--- src/javascripts/componenets/bearData.js
+++ src/javascripts/componenets/bearData.js
@@ -1,23 +1,38 @@
 const bears = [
   {
+    id: 0,
     name: 'Fozzy',
-    imgUrl: 'http://3.bp.blogspot.com/_Dp2Dd4Y_YuE/SJkMdNEXcUI/AAAAAAAAARg/jfUq7PXiiS0/s400/Brown+bear+piscivory+blog+copy.jpg',
+    imgUrl: 'http://3.bp.blogspot.com/_Dp2Dd4Y_YuE/SJkMdNEXcUI/AAAAAAAAARg/jfUq7PXiiS0/s400/Brown+bear+piscivory+blog',
+    caught: 2,
+    missed: 5,
   },
   {
+    id: 1,
     name: 'Teddy',
     imgUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTanjD8u20zCc7UBGr62bDgqTvVuNt_uwotLg&usqp=CAU',
+    caught: 2,
+    missed: 5,
   },
   {
+    id: 2,
     name: 'Smokey',
     imgUrl: 'https://i.pinimg.com/originals/04/c1/fd/04c1fd5d24366794970a49edbc57eeb0.jpg',
+    caught: 2,
+    missed: 5,
   },
   {
+    id: 3,
     name: 'Fuzzy Wuzzy',
     imgUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkxekTymMIHA0cFD1TBY9mMbxzEnHdQwWF0w&usqp=CAU',
+    caught: 2,
+    missed: 5,
   },
   {
+    id: 4,
     name: 'Terrance',
     imgUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuFnV11UZ1V0TF26NKUiBVczkPfYAldX8AMA&usqp=CAU',
+    caught: 2,
+    missed: 5,
   },
 ];
 
---
+++ src/javascripts/componenets/river.js
@@ -0,0 +1,49 @@
+import renderToDom from './helpers';
+
+const bears = [];
+
+const createBearCards = () => {
+  let domString = '';
+  bears.forEach((bear, i) => {
+    domString += `<div class="card my-2" style="width: 12rem; margin: .5em;" id=${i}>
+                    <img class="card-img-top" src=${bear.imgUrl}"/>
+                      <div class="card-body">
+                        <center><h5 class="card-text">${bear.name}</h5></center>
+                        <hr>
+                      <button type="button" class="btn btn-light fish-btn" id="${bear.fishBtn}"><img src="https://cdn3.iconfinder.com/data/icons/glypho-signs/64/fish-fishing-512.png" style="width: 20%";></button>
+                      <hr>
+                      <p id="caught">Caught: ${bear.caught}</p>
+                      <p id="missed">Missed: ${bear.missed}</p>
+                    </div>
+                  </div>`;
+  });
+
+  renderToDom('#river', domString);
+};
+
+const getFormInfo = (e) => {
+  e.preventDefault();
+  const name = document.querySelector('#bear-name-input').value;
+  const imgUrl = document.querySelector('#bear-url-input').value;
+  const caught = '';
+  const missed = '';
+
+  const obj = {
+    name,
+    imgUrl,
+    caught,
+    missed,
+  };
+
+  bears.push(obj);
+  createBearCards(bears);
+
+  document.querySelector('form').reset();
+};
+
+const handleButtonEvents = () => {
+  document.querySelector('#submit').addEventListener('click', getFormInfo);
+  document.querySelector('.fish-btn').addEventListener('click', catchFish);
+};
+
+export default handleButtonEvents;
