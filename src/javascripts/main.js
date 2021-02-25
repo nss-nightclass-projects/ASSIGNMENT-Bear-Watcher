4fdfdd0fee967958267ef7ebb56468cb40af7dee
Ben Jarrett <b.cl.jarrett@gmail.com>
2021-02-24 20:24:58 -0600

    Finished All Setup Steps

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,5 +1,62 @@
 import '../styles/main.scss';
 
+const bearsArr = [
+  {
+    bearName: 'Billie',
+    descriptionOfBear: '6ft: 570 lb',
+    imageURL: 'https://lh3.googleusercontent.com/proxy/rO3Dyc06Lkwa7uq5aeeig09rHkgP9HJwA-pIRMG-lfVG8G16aRFwxRAAdJDlKcBE_E-EANzrr6EXqH0zh4WU-aw1tH67LLwu0bxPIZ_5WZ66nHQxbtb5Ddk4xIcMuvX1',
+
+  },
+  {
+    bearName: 'Jack',
+    descriptionOfBear: '7ft: 670 lb',
+    imageURL: 'https://lh3.googleusercontent.com/proxy/dSnAtF0q1mFFUTBFaoYG4uQNx-rZwGii4Mnm5y6G5GElMD_kGqB_nrON0LokghXbEvhw8Cjy1gGjrjoMdZdZxPyvF1JLqvs_qheZ3su56wMAo59YoZMf-4E',
+
+  },
+  {
+    bearName: 'Keith',
+    descriptionOfBear: '4ft: 876 lb',
+    imageURL: 'https://scienmag.com/wp-content/uploads/2019/12/Learning-from-the-bears.jpg',
+
+  },
+  {
+    bearName: 'Rick',
+    descriptionOfBear: '8ft: 670 lb',
+    imageURL: 'https://travel.home.sndimg.com/content/dam/images/travel/stock/2018/2/19/0/shutterstock_34956754_grizzly-bear.jpg.rend.hgtvcom.1280.914.suffix/1519070779400.jpeg',
+
+  },
+  {
+    bearName: 'Carlos',
+    descriptionOfBear: '8ft: 670 lb',
+    imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKRGUXrJAB2Kez2ojPgDOz2Nn4RVY6WSw69Q&usqp=CAU',
+
+  },
+  {
+    bearName: 'Nick',
+    descriptionOfBear: '8ft: 670 lb',
+    imageURL: 'https://geographical.co.uk/media/k2/items/cache/6bd6755a4e7a13862647f8a77aaf84b9_XL.jpg',
+
+  },
+  {
+    bearName: 'Mike',
+    descriptionOfBear: '8ft: 670 lb',
+    imageURL: 'https://lh3.googleusercontent.com/proxy/dAB6BX9KNVAhEcfH27a691ok3l4kH9Cz0kB_twl-otbRGqgjgEJaWZhZ_JUqaLgjfcBbJ-CecG_bsm_RBteIAAEleYS-JdEaUBJ1xKxIRmUZB4FJIbegrn8p5eQmx9d7frZ22bxDRUdl1h8r5kgjEOHcU2AnEMhthLTj',
+
+  },
+  {
+    bearName: 'Rob',
+    descriptionOfBear: '8ft: 670 lb',
+    imageURL: 'https://www.alaskawaste.com/assets/images/uploads/anchorage/new-bear-photo.jpg',
+
+  },
+  {
+    bearName: 'Jared',
+    descriptionOfBear: '8ft: 670 lb',
+    imageURL: 'https://imgs.mongabay.com/wp-content/uploads/sites/20/2020/04/07113107/Moon-bear-at-the-sanctuary-_credit-Free-the-Bears.jpg',
+
+  },
+];
+
 const init = () => {
   $('#app').html('<h1>HELLO! You are up and running!</h1>');
   console.log('YOU ARE UP AND RUNNING!');
